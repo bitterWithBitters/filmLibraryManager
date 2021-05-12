7fbb57ff8fa8e9a6e2182cce13c1e63f70183b40
bitterWithBitters <83998703+bitterWithBitters@users.noreply.github.com>
2021-05-12 16:01:40 -0600

    Update findFilmsWithoutPosters.py

--- findFilmsWithoutPosters.py
+++ findFilmsWithoutPosters.py
@@ -5,47 +5,68 @@ breaktest=0
 filmsAll = []
 filmsWithPosters = []
 filmsWithoutPosters = []
+filmsWithLongNamePosters = []
 
 posterPath = ""
 
 folderString = "/Volumes/video/Biblioteca_2019_01_17/Movies/Doc*"
-posterString = "/Volumes/video/Biblioteca_2019_01_17/Movies/Doc*/poster.*"
+posterString = folderString + "/poster.*"
+posterLongNameString = folderString + "/*-poster.*"
 
 print('Start\n')
 
 print("filmsAll:")
 
-for folderName in glob.glob(folderString) :
+for filmName in glob.glob(folderString) :
     breaktest = breaktest+1
-    if breaktest>10: break
-    print(folderName)
-    filmsAll.append(folderName)
+    if breaktest>50: break
+    print(filmName)
+    filmsAll.append(filmName)
 
 breaktest=0
 
 print("\nfilmsWithPosters:")
 
-for posterName in glob.glob(posterString) :
+for filmName in glob.glob(posterString) :
     breaktest = breaktest+1
-    if breaktest>10: break
-    posterPath = "/"
-    posterPath += posterName.strip("/poster.jpg")
+    if breaktest>50: break
+    if filmName.endswith("/poster.jpg") :
+        posterPath = filmName[:-11]
     print(posterPath)
+    # print(filmName)
     filmsWithPosters.append(posterPath)
 
 breaktest=0
 
-filmsWithoutPosters = list(set(filmsAll).difference(filmsWithPosters))
+print("\nfilmsWithLongNamePosters:")
 
-# print("\nfilmsAll")
-# for i in filmsAll :
-#     print(i)
+for filmName in glob.glob(posterLongNameString) :
+    breaktest = breaktest+1
+    if breaktest>50: break
+    if filmName.endswith("-poster.jpg"):
+        # print("Yep!") 
+        posterPath = filmName.rpartition('/') [0]
+    # print(filmName)
+    print(posterPath)
+    filmsWithLongNamePosters.append(posterPath)
 
-# print("\nfilmsWithPosters")
-# for i in filmsWithPosters :
-#     print(i)
+breaktest=0
 
-print("\nfilmsWithoutPosters")
+filmsWithoutPosters = list(set(filmsAll).difference(filmsWithPosters).difference(filmsWithLongNamePosters))
+
+#print("\nfilmsAll LIST")
+#for i in filmsAll :
+#    print(i)
+
+#print("\nfilmsWithPosters LIST")
+#for i in filmsWithPosters :
+#    print(i)
+
+#print("\nfilmsWithLongNamePosters LIST")
+#for i in filmsWithLongNamePosters :
+#    print(i)
+
+print("\nfilmsWithoutPosters LIST")
 for i in filmsWithoutPosters :
     print(i)
 
